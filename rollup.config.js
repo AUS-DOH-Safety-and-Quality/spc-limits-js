de84e61d22f7d240c0ce9a7c107ccb72174bbc89
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-05-27 03:58:55 +0300

    Add config for bundling

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+// rollup.config.js
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'index.ts',
+  output: {
+    dir: 'output',
+    format: 'iife'
+  },
+  plugins: [typescript(), nodeResolve(), commonjs()]
+};
